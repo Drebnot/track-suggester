93065e73a328a598629147ab73c035bbe65ac57f
Jesse <jessedrebin@gmail.com>
2018-03-23 16:40:21 -0700

    many tweaks. removed unnecessary form and replaceed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(function(){
   $('form#text-form').submit(function(event){
     event.preventDefault();
-    var name = $('text-input').val();
-    var work = $("input:radio[name=work]:checked").val();
+    var description = $('text-input').val();
+    var question1 = $("input:radio[name=work]:checked").val();
+    var question2 = $("input:radio[name=work]:checked").val();
   });
 });
