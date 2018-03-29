933331050daf735cc8bb4bf2f4da3a23026fa978
Jesse <jessedrebin@gmail.com>
2018-03-29 14:03:15 -0700

    added new var answer

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     var question3 = parseInt $("input:radio[name=question3]:checked").val();
     var question4 = parseInt $("input:radio[name=question4]:checked").val();
     var question5 = parseInt $("input:radio[name=question5]:checked").val();
-
+    var answer = question1 + question2 + question3 + question4 + question5;
 
     if ((question1 === "CSS") && (question2 === "CSS")) {
       $("#tracksuggestion").text("CSS");
