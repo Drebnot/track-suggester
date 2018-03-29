1fee4941910c0a439bbf4b77a9f7307cc38b8044
Jesse <jessedrebin@gmail.com>
2018-03-29 14:46:12 -0700

    modify if statement to < for numeric value

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $("main-form").submit(function(event) {
     event.preventDefault();
 
-    var question1 = parseInt $("input:radio[name=question1]:checked").val();
+    var question1 = parseInt ($("input:radio[name=question1]:checked").val());
     var question2 = parseInt $("input:radio[name=question2]:checked").val();
     var question3 = parseInt $("input:radio[name=question3]:checked").val();
     var question4 = parseInt $("input:radio[name=question4]:checked").val();
     var question5 = parseInt $("input:radio[name=question5]:checked").val();
     var answer = question1 + question2 + question3 + question4 + question5;
 
-    if ((question1 === "CSS") && (question2 === "CSS")) {
-      $("#tracksuggestion").text("CSS");
-    } else if ((question1 === "C#") && (question2 === "C#")) {
-      $("track-suggestion").text("C#");
-    } else if ((question1 === "Java") && (question2 === "Java")) {
-      $("track-suggestion").text("Java");
-    }
 
+    if (answer <=5) {
+      $("#css").show();
+    }
 
     $("#button").click(function() {
       $("track-suggestion").show
