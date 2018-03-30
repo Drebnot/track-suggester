67114868fa60e8a457c450bba1d5d4f50e8d4e52
Jesse <jessedrebin@gmail.com>
2018-03-29 21:59:58 -0700

    add parentheses to parseInt variables

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     event.preventDefault();
 
     var question1 = parseInt ($("input:radio[name=question1]:checked").val());
-    var question2 = parseInt $("input:radio[name=question2]:checked").val();
-    var question3 = parseInt $("input:radio[name=question3]:checked").val();
-    var question4 = parseInt $("input:radio[name=question4]:checked").val();
-    var question5 = parseInt $("input:radio[name=question5]:checked").val();
+    var question2 = parseInt ($("input:radio[name=question2]:checked").val());
+    var question3 = parseInt ($("input:radio[name=question3]:checked").val());
+    var question4 = parseInt ($("input:radio[name=question4]:checked").val());
+    var question5 = parseInt ($("input:radio[name=question5]:checked").val());
     var answer = question1 + question2 + question3 + question4 + question5;
 
 
